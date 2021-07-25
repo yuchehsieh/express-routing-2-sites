bfe5b918107ea814a3708dff2cb4107646b2007a
Allen Chen (Invoker) <e2ne00@gmail.com>
2021-07-25 14:05:18 +0800

    fix: frontend route's order, some annotation

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || "5000";
 
+/* Frontend:
+    frontend route must be place at the first 
+*/
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/index.html'))
+})
+
+/* Backend:
+    backend route
+*/
 app.get('/backstage', (req, res) => {
     res.sendFile(path.join(__dirname, '/backend_cms/build/index.html'))
 });
@@ -11,10 +21,9 @@ app.get('/backstage/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/backend_cms/build/index.html'))
 });
 
+/* Backend:
+    backend resources
+*/
 app.use(express.static(path.join(__dirname + "/backend_cms/build")))
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/frontend/index.html'))
-})
 
 app.listen(port);
