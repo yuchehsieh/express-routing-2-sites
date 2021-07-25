37d13bf698a769a41d442f46b5641ca63a48c751
Allen Chen (Invoker) <e2ne00@gmail.com>
2021-07-25 12:07:48 +0800

    node app initial setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || "5000";
+
+app.get('/backstage', (req, res) => {
+    res.sendFile(path.join(__dirname, './backend_cms/build/index.html'))
+});
+
+app.get('/backstage/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './backend_cms/build/index.html'))
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/index.html'))
+})
+
+app.listen(port);
